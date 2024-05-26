34c36404a6da365cdc38489e7d5b48441d0d2a48
MAHANNAVIDU577 <163949432+MAHANNAVIDU577@users.noreply.github.com>
2024-05-26 18:52:28 +0530

    job details

--- jobportal/routes/web.php
+++ jobportal/routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\HomeController;
 // });
 
 Route::get('/', [HomeController::class,'index'])->name('home');
-
+Route::get('/jobs', [HomeController::class,'index'])->name('jobs');
 
 
 Route::group(['prefix' => 'account'],function(){
